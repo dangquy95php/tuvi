ab8925b5fca2325f7ae6c446dda89a4534a1a99d
dangquy95php@gmail.com <dangquy95php@gmail.com>
2020-12-21 18:38:17 +0700

    table and data

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('users');
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id')->id();
             $table->string('name');
+            $table->string('role')->default(1);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('status')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
